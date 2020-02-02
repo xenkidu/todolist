fc6cf9d71ce9ec5ef9bf0c6f25fdbca57e21e57c
xenkidu <aburton85@live.com>
2020-02-01 18:08:12 -0700

    refactored urls

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from lists import views as list_views
 from lists import urls as list_urls
 
