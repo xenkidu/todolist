ed10191e5faad2a0e53863272a4366a47950da59
xenkidu <aburton85@live.com>
2020-02-04 12:41:21 -0700

    configured urls for static serving

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from lists import views as list_views
 from lists import urls as list_urls
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
